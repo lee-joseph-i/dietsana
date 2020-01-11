afef3fe31d08387977458662425d848c6ee3e701
Joseph Lee <josephinlee91@gmail.com>
2020-01-10 17:58:21 -0800

    change components from jsx to js

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,8 +5,6 @@ import LandingContainer from './landing/landing_container';
 import Main from './main/main_container';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import Footer from './nav_bar/footer';
-import SignupContainer from './session/signup_container';
-import LoginContainer from './session/login_container';
 import Modal from './modal/modal';
 
 const App = () => (
