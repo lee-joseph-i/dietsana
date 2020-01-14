c2ca6f3b9746f7252531d660c9aa14cdfd2a58ff
Joseph Lee <josephinlee91@gmail.com>
2020-01-14 12:00:50 -0800

    fix associations naming, fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,42 +12,42 @@ ApplicationRecord.transaction do
   User.destroy_all
   Project.destroy_all
 
-  User.create(
+  User.create!(
     first_name: 'Joseph', 
     last_name: 'Lee', 
     email: 'josephlee@dietsana.com', 
     password: "password123")
 
-  User.create(
+  User.create!(
     first_name: 'John', 
     last_name: 'Lee', 
     email: 'johnlee@dietsana.com', 
     password: "password123")
 
-  User.create(
+  User.create!(
     first_name: 'Chris', 
     last_name: 'Lee', 
     email: 'chrislee@dietsana.com', 
     password: "password123")
 
-  Project.create(
+  Project.create!(
     name: 'Full Stack Project',
-    creator_id: 1,
     owner_id: 1,
+    creator_id: 1,
     description: 'Build your first (but not last) FS project - from scratch!',
   )
 
-  Project.create(
+  Project.create!(
     name: 'Flex Project',
-    creator_id: 2,
     owner_id: 1,
+    creator_id: 2,
     description: 'Build a project using the MERN stack with a team.',
   )
 
-  Project.create(
+  Project.create!(
     name: 'Javascript Project',
-    creator_id: 3,
     owner_id: 1,
+    creator_id: 3,
     description: 'Build your own game using JS! pew pew~',
   )
 
