12d26ca151654ea9adc716ca9fe84e94de1ca11c
Joseph Lee <josephinlee91@gmail.com>
2020-05-26 13:56:43 -0700

    section rb methods

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,10 +1,20 @@
 class Section < ApplicationRecord
   validates :name, :project_id, presence: true
-
   belongs_to :project,
-    foreign_key: :project_id
-
   has_many :tasks,
-    foreign_key: :section_id
+
+  def add_task(task_id)
+    # task methods
+    self.task_will_change!
+    self.task.push(task_id)
+    self.save
+  end
+
+  def move_task(old_idx, new_idx)
+    # moving tasks from section to section
+    self.task_will_change!
+    self.task.insert(new_idx, self.task.delete_at(old_idx))
+    self.save
+  end
 
 end
