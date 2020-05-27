56248f9bb57ff3d16155e81f1ef6aafa8d94a144
Joseph Lee <josephinlee91@gmail.com>
2020-05-27 11:10:33 -0700

    tasks controller

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,18 +1,39 @@
 class Api::TasksController < ApplicationController
-    def create
+  
+  def index
+    # established that tasks cannot exist without a section... may need to check my model/db validations
+    @tasks = Section.find_by(id: params[:section_id]).project.tasks
+  end
+  
+  def create
     @task = Task.new(task_params)
+    @task.column_id = @task.column_id
+    @task.creator_id = current_user.id
 
     if @task.save
       # login(@user)
       # render "api/tasks/show"
+      render :show
     else
-      render json: ['Please include a title.'], status: 422
+      # render json: ['Please include a title.'], status: 422
+      render json: @task.errors.full_messages, status: 401
     end
   end
 
-  def index
-    @tasks = Task.all
-    # this may need to be configured to index all tasks associated with the project
+  def show
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    @task.subtask_will_change!
+    @task.update_attributes(task_params)
+    render :update
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])
+    @task.destroy
   end
 
   private
