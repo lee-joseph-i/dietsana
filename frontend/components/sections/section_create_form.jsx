6c761d0476d77e35ce8333f38bed2aff6e903ea1
Joseph Lee <josephinlee91@gmail.com>
2020-05-31 23:54:57 -0700

    react hooks setup

--- frontend/components/sections/section_create_form.jsx
+++ frontend/components/sections/section_create_form.jsx
@@ -1,1 +1,17 @@
 // setup using React hooks! 
+
+import React, { useState } from 'react';
+
+// function Example() {
+//   // Declare a new state variable, which we'll call "count"
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>
+//         Click me
+//       </button>
+//     </div>
+//   );
+// }
