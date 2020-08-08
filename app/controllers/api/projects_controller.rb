365cbfe5e707d9e967701de86bf2c48967c542cd
Joseph Lee <josephinlee91@gmail.com>
2020-08-07 20:37:28 -0700

    test backend

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -27,8 +27,8 @@ class Api::ProjectsController < ApplicationController
 
   def update
     @project = Project.find(params[:id])
-    @project.section_will_change! # Joseph-Review
-    project_params[:section] = [] unless project_params[:section] # Joseph-Review
+    @project.section_will_change! # Joseph-Review -- Test backend 
+    project_params[:section] = [] unless project_params[:section] # Joseph-Review -- Test backend
     if @project.update(project_params)
           render :show
     else
